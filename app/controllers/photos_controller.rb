762d6cc025b8e115438dfc3b14db2df9fd1e6302
Kazuya NUMATA <numata@gmail.com>
2014-07-05 16:00:28 +0900

    Add missing some stuffs for form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -69,6 +69,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:name, :avatar)
+      params.require(:photo).permit(:name, :avatar, :avatar_cache)
     end
 end
